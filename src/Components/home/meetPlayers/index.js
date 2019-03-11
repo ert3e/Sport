2818d66698e6dcac3e06d62de45ebe67cd168ed9
ert3e <samoilenkod@mail.ua>
2019-03-11 23:23:27 +0200

    Add animation to meet players_card

--- src/Components/home/meetPlayers/index.js
+++ src/Components/home/meetPlayers/index.js
@@ -2,22 +2,32 @@ import React, { Component } from 'react';
 import Stripes from '../../../Resources/images/stripes.png';
 import { Tag } from '../../ui/misc';
 import Reveal from 'react-reveal/Reveal';
+import HomeCards from './cards';
 
 class MeatPlayers extends Component {
     state = {
-
+        show:false
     }
 
     render() {
         return (
-            <Reveal>
+            <Reveal
+                fraction={0.7}
+                onReveal={() => {
+                    this.setState({
+                        show:true
+                    })
+                }}
+            >
                 <div className="home_meetplayers"
-                    style={{background:`#ffffff url(${Stripes})`}}  
-                    >
+                    style={{ background: `#ffffff url(${Stripes})` }}
+                >
                     <div className="container">
                         <div className="home_meetplayers_wrapper">
                             <div className="home_card_wrapper">
-                                card
+                                <HomeCards
+                                    show={this.state.show}
+                                />
                             </div>
                             <div className="home_text_wrapper">
                                 <div>
@@ -71,7 +81,7 @@ class MeatPlayers extends Component {
                                             marginBottom: '27px',
                                             border: '1px solid #0e1731'
                                         }}
-                                        >
+                                    >
                                         Meet them here
                                     </Tag>
                                 </div>
