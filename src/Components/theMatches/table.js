1698481b61f248cfa3de6fc36c05656fddf9dff5
ert3e <samoilenkod@mail.ua>
2019-04-16 23:38:26 +0300

    Add componentdidmout and team postion method

--- src/Components/theMatches/table.js
+++ src/Components/theMatches/table.js
@@ -18,8 +18,31 @@ const style = {
 }
 class LeagueTable extends Component {
     state = {
-        positio:[]
+        positions:[]
     }
+    componentDidMount(){
+        firebseDB.ref('position').once('value').then((snapshot) =>{
+            const position = firebaseLooper(snapshot);
+
+            this.setState({
+                position:position
+            })
+        })
+    }
+    showTeamposition = (pos) =>(
+        pos ?
+        pos.map((pos,i)=>(
+            <TableRow key={i}>
+                <TableCell style={style.cell}>{i+1}</TableCell>
+                <TableCell style={style.cell}>{pos.team}</TableCell>
+                <TableCell numeric style={style.cell}>{pos.w}</TableCell>
+                <TableCell numeric style={style.cell}>{pos.d}</TableCell>
+                <TableCell numeric style={style.cell}>{pos.l}</TableCell>
+                <TableCell numeric style={style.cell}>{pos.pts}</TableCell>
+            </TableRow>
+        ))
+        :null
+    )
     render() {
         return (
             <div>
